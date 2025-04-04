2195d697e8467f6f8d4be8600ceb01b60f1be503
hache0099 <hache0099_github@protonmail.com>
2025-04-04 20:59:34 -0300

    Añadidas rutas para login y logout de usuario

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+/*Route::prefix('v1')->group(function () {
     Route::apiResource('auth', AuthController::class)->names('auth');
-});
+});*/
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');

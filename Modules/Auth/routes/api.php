eb3196e56a9be6c2e13f4e4ca65e34140f8a221c
hache0099 <hache0099_github@protonmail.com>
2025-04-06 21:46:24 -0300

    Cambiado el prefijo de las rutas

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-/*Route::prefix('v1')->group(function () {
+Route::prefix('v1/auth')->group(function () {
     Route::apiResource('auth', AuthController::class)->names('auth');
-});*/
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+});
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+

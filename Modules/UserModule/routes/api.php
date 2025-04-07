ac569de9e59f66e4dafd82ae17451eb06116fd49
hache0099 <hache0099_github@protonmail.com>
2025-04-06 21:46:41 -0300

    Añadido middleware

--- Modules/UserModule/routes/api.php
+++ Modules/UserModule/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\UserModule\Http\Controllers\UserModuleController;
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('user', UserModuleController::class)->names('user');
 });

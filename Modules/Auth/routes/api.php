c60fb16648d9092b8e2c13fa06cc721ad5cf125c
hache0099 <hache0099_github@protonmail.com>
2025-04-11 17:02:07 -0300

    Añadidas funciones para registrar a un nuevo usuario

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -8,6 +8,12 @@ Route::prefix('v1/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+    Route::post('/register', [AuthController::class, 'register']);
+
+
+    Route::get('/get-tablas-maestras', [AuthController::class, 'getTablasMaestras']);
+    Route::get('/get-tipo-documento', [AuthController::class, 'getTiposDocumento']);
+    Route::post('/check-email', [AuthController::class, 'checkEmailExists']);
 });
 
 

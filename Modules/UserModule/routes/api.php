5d996ecdc291f2526a90b16ff8c1e5f6132cdfb9
hache0099 <hache0099_github@protonmail.com>
2025-04-11 17:03:40 -0300

    Saqué el middleware de auth para pruebas

--- Modules/UserModule/routes/api.php
+++ Modules/UserModule/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\UserModule\Http\Controllers\UserModuleController;
 
-Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+Route::prefix('v1')->group(function () {
     Route::apiResource('user', UserModuleController::class)->names('user');
 });

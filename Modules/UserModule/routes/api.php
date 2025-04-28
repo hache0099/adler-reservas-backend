cb93181c4e4fd338e47e64ba994e07acf426bad9
hache0099 <hache0099_github@protonmail.com>
2025-04-28 17:31:30 -0300

    Añadida nueva ruta para el módulo User

--- Modules/UserModule/routes/api.php
+++ Modules/UserModule/routes/api.php
@@ -5,4 +5,5 @@ use Modules\UserModule\Http\Controllers\UserModuleController;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('user', UserModuleController::class)->names('user');
+    Route::put('/user/update-personal-info/{id}', [UserModuleController::class, 'updatePersonalInfo']);
 });

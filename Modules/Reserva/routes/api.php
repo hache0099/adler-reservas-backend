7a6aedd5dcce4371bc793d80de1204c8070e140e
hache0099 <hache0099_github@protonmail.com>
2025-04-28 17:22:26 -0300

    Añadidas nuevas rutas para el módulo Reservas

--- Modules/Reserva/routes/api.php
+++ Modules/Reserva/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Reserva\Http\Controllers\ReservaController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+Route::prefix('v1/reservas')->group(function () {
     Route::apiResource('reserva', ReservaController::class)->names('reserva');
+    
+    Route::get('/{id}/get-pendientes', [ReservaController::class, 'getReservaPendienteByUser']);
+    Route::get('/get-canchas-disponibles',[ReservaController::class, 'getCanchasDisponibles']);
 });

d11a0448d069dce95d2e3ded6f08ea34b3ed34d2
hache0099 <hache0099_github@protonmail.com>
2025-04-28 18:24:10 -0300

    Añadidas funciones para devolver modulos

--- Modules/Gestion/app/Http/Controllers/GestionController.php
+++ Modules/Gestion/app/Http/Controllers/GestionController.php
@@ -5,7 +5,7 @@ namespace Modules\Gestion\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\{TipoDocumento, TipoContacto, TipoDomicilio};
+use App\Models\{Modulo, TipoDocumento, TipoContacto, TipoDomicilio, TipoCancha};
 
 class GestionController extends Controller
 {
@@ -22,5 +22,29 @@ class GestionController extends Controller
     public function getTiposDomicilio()
     {
         return TipoDomicilio::where('estado', 1)->get(['id', 'descripcion']);
+    }
+
+    public function getTiposCancha()
+    {
+        return TipoCancha::where('estado', 1)->get(['id', 'material']);
+    }
+
+    public function getModulos(){
+        $rechazados = [
+            'gestion indice',
+            'gestion socios',
+            'gestion de fotos de canchas',
+            'gestion horarios de canchas',   
+            'gestion de empleados',
+        ];
+        return Modulo::where('ruta','like','/gestion/%')
+        ->get(['id', 'descripcion', 'ruta'])
+        ->reject(function ($modulo) use ($rechazados){
+            return in_array(strtolower($modulo->descripcion), $rechazados);
+        });
+    }
+
+    public function getAllModulos(){
+        return Modulo::get(['id', 'descripcion', 'ruta']);
     }
 }

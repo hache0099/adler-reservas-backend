38fe8d52aea080287f4b85acec2692528f784707
hache0099 <hache0099_github@protonmail.com>
2025-04-28 17:20:03 -0300

    Creados nuevos tests

---
+++ tests/Feature/GestionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GestionTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_get_modulos(): void
+    {
+        $response = $this->get('/api/v1/gestion/get-modulos');
+
+        dd($response->json());
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/ReservaTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ReservaTest extends TestCase
+{
+    public function test_get_canchas(): void
+    {
+        $response = $this->get('/api/v1/reservas/get-canchas-disponibles?fecha=2025-04-22&hora=22');
+
+        //dd($response->json());
+        $response->assertStatus(200);
+    }
+
+
+    public function test_hora_imposible()
+    {
+        $response = $this->get('/api/v1/reservas/get-canchas-disponibles?fecha=2025-04-22&hora=05');
+
+        //dd($response->json());
+        $response->assertStatus(200);
+    }
+}
